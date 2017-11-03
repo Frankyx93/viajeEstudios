d65a4efa6b70e609bf4741855d6faa16452cd674
Fran Marcos <franmarcos93@gmail.com>
2017-11-03 10:15:20 +0100

    login de usuario

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,19 @@
 'use strict';
 
+var dsConfig = require('../datasources.local.js');
+var path = require('path');
+
 module.exports = function(server) {
-  // Install a `/` route that returns server status
+
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
-};
+
+  //login page
+  router.get('/', function(req, res) {
+    var credentials = dsConfig.myEmailDatasource.transports[0].auth;
+    res.render('login', {
+      email: credentials.user,
+      password: credentials.pass
+    });
+  });
+    server.use(router);
+  };
